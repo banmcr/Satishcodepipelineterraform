79c71613b2d18b1933509e3cab1e760543648d29
Bhagyesh <bhagyesh.machhar@cloud4c.com>
2025-06-11 22:48:34 +0530

    add ssm parameter to buildspec

--- Main_infra/provider.tf
+++ Main_infra/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
   assume_role {
-    role_arn = "arn:aws:iam::682033488423:role/codepipeline-Terraform-Role"
+    role_arn = "arn:aws:iam::${var.accounts}:role/codepipeline-Terraform-Role"
   }
 }
