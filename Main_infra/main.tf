33fa114164585fbf6834a0ee437daa218dc12fa2
Bhagyesh <bhagyesh.machhar@cloud4c.com>
2025-06-11 21:48:52 +0530

    adding trivy 2

--- Main_infra/main.tf
+++ Main_infra/main.tf
@@ -4,19 +4,20 @@ data "aws_region" "current" {}
 resource "aws_security_group" "name" {
     name = "asd"
     vpc_id = "vpc-07b5343a7c443a28b"
+    description = "asdsad"
     ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port        = "-1"
+    to_port          = "-1"
+    protocol         = "icmp"
+    cidr_blocks      = ["1.1.1.1/32"]
+
   }
     egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port        = "-1"
+    to_port          = "-1"
+    protocol         = "icmp"
+    cidr_blocks      = ["1.1.1.1/32"]
+
   }
 }
 
