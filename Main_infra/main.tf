3a4530c83ad4b3213f1883e1d8e87d3e1ec301d1
Bhagyesh <bhagyesh.machhar@cloud4c.com>
2025-06-12 08:40:20 +0530

    adding role and attach

--- Main_infra/main.tf
+++ Main_infra/main.tf
@@ -22,6 +22,35 @@ resource "aws_iam_policy" "policy" {
   })
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "my_test_Attachement" {
+  role = aws_iam_role.test_role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
 output "regionname" {
   value = data.aws_region.current.id
 }
