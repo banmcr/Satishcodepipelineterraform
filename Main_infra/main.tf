1a6f20664d302ca94ad8176c3825257096fc10de
Bhagyesh <bhagyesh.machhar@cloud4c.com>
2025-06-13 08:44:44 +0530

    asd

--- Main_infra/main.tf
+++ Main_infra/main.tf
@@ -1,6 +1,28 @@
 
 data "aws_region" "current" {}
 
+locals {
+  mytime = timestamp()
+  name = "${var.name}-${local.mytime}"
+}
+
+resource "aws_iam_role" "asdasghaf" {
+  name = local.name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_policy" "policy" {
   name        = "test_policy"
   path        = "/"
--- Main_infra/variables.tf
+++ Main_infra/variables.tf
@@ -1,3 +1,14 @@
 variable "account_id" {
   type = string
 }
+
+variable "name" {
+  type = string
+  description = "Name of the resource to be created"
+  default = "my-aws-resource"
+  sensitive = true
+  validation {
+    condition = can(regex("^[a-z0-9-]+$", var.name))
+    error_message = "Name has to be small letter only"
+  }
+}
